57e30cabc1baf6e2648c273d6f57bca830e8349b
ZSR22 <zasari@ZURINOLAPTOP.localdomain>
2025-05-30 02:06:00 +0200

    Aggiunta file Report.c e report.h

---
+++ report.h
@@ -0,0 +1,17 @@
+/*
+=============================================================================
+// File: report.h
+ Descrizione: Header per la generazione di report mensili e salvataggio su file
+ Autore: Salvatore Zurino
+ Data: 29/05/2025
+ Versione: 1.0
+=============================================================================
+*/
+#include <stddef.h>
+#include "Prenotazioni.h"
+#ifndef REPORT_H
+#define REPORT_H
+
+void genera_report_mensile(const Prenotazione* prenotazioni, size_t numero_prenotazioni);
+
+#endif 
