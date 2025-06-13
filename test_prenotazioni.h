37194ddd7da21fceb06e0d29e64cbd244e2a34be
BulletJim <gianmarco.campania@gmail.com>
2025-06-13 16:03:25 +0200

    Completata documentazione branch di test

--- test_prenotazioni.h
+++ test_prenotazioni.h
@@ -12,23 +12,6 @@
 
 #include "header_sorgenti/Lista_Prenotazioni.h"
 
-// Test 1 – Verifica prenotazione con dati corretti
-
-// Test 2 – Verifica prenotazione con cliente inesistente
-
-// Test 3 – Verifica prenotazione con lezione piena
-
-// Test 4 – Verifica prenotazione con orari sovvrapposti
-
-// Test 5 – Lezione con ID non valido
-
-// Test 6 – Prenotazione già effettuata per la stessa lezione
-
-// Test 7 – Codice fiscale malformato (es. stringa vuota o troppo corta)
-
-// Test 8 – Prenotazione per una lezione con data già superata
-
-//Richiamata nel main, funzione di comunicazione con l'esterno
 void avvia_test_prenotazioni(Lista_Prenotazioni* lista, NodoAlbero* radice, Catalogo_Lezioni* catalogo);
 
 #endif
